90506567ae1c5b5379fb034db3f01ae68c05512e
Tirpitz9992 <tirpitz9992@gmail.com>
2024-07-14 02:14:42 +0800

    srv

--- arcane-craft-be/arcane_craft_be_server/src/arcane_craft_be_server_app.erl
+++ arcane-craft-be/arcane_craft_be_server/src/arcane_craft_be_server_app.erl
@@ -4,9 +4,20 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    db_manager:start(normal, []),
-    {ok, self()}.
+    {ok, Port} = application:get_env(arcane_craft_be_server, port),
+    {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]),
+    io:format("Server started on port ~p~n", [Port]),
+    Pid = spawn(fun() -> accept(ListenSocket) end),
+    {ok, Pid}.
 
 stop(_State) ->
-    db_manager:stop([]),
     ok.
+
+accept(ListenSocket) ->
+    {ok, Socket} = gen_tcp:accept(ListenSocket),
+    spawn(fun() -> handle(Socket) end),
+    accept(ListenSocket).
+
+handle(Socket) ->
+    gen_tcp:send(Socket, "Hello, World!\n"),
+    gen_tcp:close(Socket).
