101d3b6df1e2261aaca3dadc901a78699297c50b
Tirpitz9992 <tirpitz9992@gmail.com>
2024-07-15 02:51:28 +0800

    rename

---
+++ arcane-craft-be/be_server/src/server_app.erl
@@ -0,0 +1,23 @@
+-module(server_app).
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    {ok, Port} = application:get_env(be_server, port),
+    {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]),
+    io:format("Server started on port ~p~n", [Port]),
+    Pid = spawn(fun() -> accept(ListenSocket) end),
+    {ok, Pid}.
+
+stop(_State) ->
+    ok.
+
+accept(ListenSocket) ->
+    {ok, Socket} = gen_tcp:accept(ListenSocket),
+    spawn(fun() -> handle(Socket) end),
+    accept(ListenSocket).
+
+handle(Socket) ->
+    gen_tcp:send(Socket, "Hello, World!\n"),
+    gen_tcp:close(Socket).
---
+++ arcane-craft-be/be_server/src/server_sup.erl
@@ -0,0 +1,14 @@
+-module(server_sup).
+-behaviour(supervisor).
+
+%% Supervisor callbacks
+-export([start_link/0, init/1]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+    ChildSpecs = [
+        {blog_server, {blog_server, start_link, []}, permanent, 5000, worker, [blog_server]}
+    ],
+    {ok, { {one_for_one, 5, 10}, ChildSpecs }}.
